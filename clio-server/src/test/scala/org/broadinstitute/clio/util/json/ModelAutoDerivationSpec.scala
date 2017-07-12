0ee8d1bdad22961b2ceee88ee402776782a9e45d
Phil Shapiro <pshapiro@broadinstitute.org>
2017-07-12 14:32:58 -0400

    - another push

--- clio-server/src/test/scala/org/broadinstitute/clio/util/json/ModelAutoDerivationSpec.scala
+++ clio-server/src/test/scala/org/broadinstitute/clio/util/json/ModelAutoDerivationSpec.scala
@@ -62,7 +62,7 @@ class ModelAutoDerivationSpec
   }
 
   it should "fail to decode camel case" in {
-    case class TestClass(fieldA: Option[String], fieldC: Int)
+    case class TestClass(fieldA: Option[String], fieldD: Int)
 
     val jsonValues =
       Table(
