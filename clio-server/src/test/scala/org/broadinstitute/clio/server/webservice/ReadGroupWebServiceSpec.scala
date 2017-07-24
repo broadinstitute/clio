e7e28ab8cd896985e668162bac2be57d65d18568
Tom Lyons <tbl@broadinstitute.org>
2017-07-24 18:20:21 -0400

    Add V2 ReadGroupWebServiceSpec tests.

--- clio-server/src/test/scala/org/broadinstitute/clio/server/webservice/ReadGroupWebServiceSpec.scala
+++ clio-server/src/test/scala/org/broadinstitute/clio/server/webservice/ReadGroupWebServiceSpec.scala
@@ -20,9 +20,26 @@ class ReadGroupWebServiceSpec
     }
   }
 
+  it should "postMetadataV2" in {
+    val webService = new MockReadGroupWebService()
+    Post(
+      "/metadata/v2/barcodeA/2/libraryC/OnPrem",
+      Map("project" -> "testProject")
+    ) ~> webService.postMetadataV2 ~> check {
+      responseAs[Map[String, String]] should be(empty)
+    }
+  }
+
   it should "query with an empty request" in {
     val webService = new MockReadGroupWebService()
     Post("/query/v1", Map.empty[String, String]) ~> webService.query ~> check {
+      responseAs[Seq[String]] should be(empty)
+    }
+  }
+
+  it should "queryV2 with an empty request" in {
+    val webService = new MockReadGroupWebService()
+    Post("/query/v2", Map.empty[String, String]) ~> webService.queryV2 ~> check {
       responseAs[Seq[String]] should be(empty)
     }
   }
@@ -34,10 +51,24 @@ class ReadGroupWebServiceSpec
     }
   }
 
+  it should "queryV2 without an empty request" in {
+    val webService = new MockReadGroupWebService()
+    Post("/query/v2", Map("project" -> "testProject")) ~> webService.queryV2 ~> check {
+      responseAs[Seq[String]] should be(empty)
+    }
+  }
+
   it should "return a JSON schema" in {
     val webService = new MockReadGroupWebService()
     Get("/schema/v1") ~> webService.getSchema ~> check {
       responseAs[Json] should be(SchemaService.readGroupSchemaJson)
+    }
+  }
+
+  it should "return a V2 JSON schema" in {
+    val webService = new MockReadGroupWebService()
+    Get("/schema/v2") ~> webService.getSchemaV2 ~> check {
+      responseAs[Json] should be(SchemaService.readGroupSchemaJsonV2)
     }
   }
 }
