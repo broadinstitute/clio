f0e6590640a1fe669e659f7a0f56751b63fe2610
Daniel Moran <danxmoran@gmail.com>
2017-07-25 15:44:59 -0400

    DSDEGP-1418 Add scripts for deploying Clio in Jenkins. (#26)

---
+++ jenkins/deploy-clio-server.sh
@@ -0,0 +1,202 @@
+#!/bin/sh
+
+# turn on verbose
+set -ex
+
+# program
+PROG_NAME=$(basename $0)
+CLIO_DIR=$(cd $(dirname $(dirname $0)) && pwd)
+
+# Run this script on Jenkins to deploy Clio to a host.
+
+# Steps:
+#   Build and push a docker image for clio-server
+#   Gather configurations associated with app to deploy
+#   Render any ctmpls
+#   Copy configs to node
+#   Stop any currently running container
+#   Re-start container using new configs
+#   Verify that the new container reports the correct version
+
+# The script expects the following ENV vars set to non-null values
+#   ENV: environment to deploy to, one of "dev", "staging", or "prod"
+#   CLIO_HOST: hostname to deploy clio to
+
+# The script also supports the optional ENV vars
+#  DOCKER_TAG: docker tag to apply to the built and deployed instance
+#              if not specified, the default SNAP version is used
+#  RUN_TESTS: if set to "false", unit tests will not be run as part of
+#             building the docker image
+
+# verify required vars are set to actual values
+if [ -z "$ENV" ]
+then
+   echo "Error: ENV not set!!"
+   exit 2
+fi
+
+if [ -z "$CLIO_HOST" ]
+then
+   echo "Error: CLIO_HOST not set!!"
+   exit 2
+fi
+
+CONFIG_DIR=clio-server/config
+
+if [ ! -d "$CONFIG_DIR" ]
+then
+    echo "Missing configuration directory ($CONFIG_DIR) - Exiting "
+    exit 1
+fi
+
+# set VAULT token location
+if [ -f /etc/vault-token-dsde ]
+then
+   TOKEN_FILE="/etc/vault-token-dsde"
+elif [ -f ${HOME}/.vault-token ]
+then
+   TOKEN_FILE="${HOME}/.vault-token"
+else
+   echo "Missing VAULT TOKEN file - Exiting"
+   exit 1
+fi
+   
+# initialize vars used in multiple config templates
+# location on host where configs should get copied
+APP_DIR=${APP_DIR:-"/app"}
+# location in the clio container where configs will be mounted
+CLIO_CONF_DIR=/etc
+# location in the clio container where logs will be mounted
+CLIO_LOG_DIR=/logs
+# name of the application config used by the clio instance
+CLIO_APP_CONF=clio.conf
+# name of the logback config used by the clio instance
+CLIO_LOGBACK_CONF=clio-logback.xml
+# name of the env file used to set up clio with docker-compose
+CLIO_ENV_FILE=clio.env
+# port to expose for clio on the host VM
+HOST_CLIO_PORT=80
+# port to expose for clio in the clio container
+CONTAINER_CLIO_PORT=${HOST_CLIO_PORT}
+
+# SSH options
+SSH_USER=jenkins
+SSHOPTS="-o UserKnownHostsFile=/dev/null -o CheckHostIP=no -o StrictHostKeyChecking=no"
+SSHCMD="ssh $SSHOPTS ${SSH_USER}@${CLIO_HOST}"
+SCPCMD="scp $SSHOPTS"
+
+# sbt command to disable tests in the docker build
+if [[ "$RUN_TESTS" = "false" ]]; then
+    SET_TESTS="set test in assembly in LocalProject(\"clio-server\") := {}"
+fi
+# sbt command to override the default docker tag
+if [[ ! -z "$DOCKER_TAG" ]]; then
+    SET_VERSION="set version := \"$DOCKER_TAG\""
+fi
+
+# build the clio-server docker image
+docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v ${CLIO_DIR}:/clio \
+    -w="/clio" \
+    broadinstitute/scala:scala-2.11.8 sbt "$SET_TESTS" "$SET_VERSION" "clio-server/docker"
+
+# if no tag was given, read the default tag from the generated resource file
+if [[ -z "$DOCKER_TAG" ]]; then
+    DOCKER_TAG=$(cat ${CLIO_DIR}/clio-server/target/scala-2.12/resource_managed/main/clio-server-version.conf | perl -pe 's/clio\.server\.version:\s+//g')
+fi
+
+# push the docker image
+docker push broadinstitute/clio-server:${DOCKER_TAG}
+
+# TMPDIR
+TMPDIR=$(mktemp -d ${CLIO_DIR}/${PROG_NAME}-XXXXXX)
+# temporary env file for rendering ctmpls
+CTMPL_ENV_FILE="${TMPDIR}/env-vars.txt"
+
+# populate temp env.txt file with necessary environment
+for var in "ENV" "DOCKER_TAG" "APP_DIR" "CLIO_CONF_DIR" "CLIO_LOG_DIR" "CLIO_APP_CONF" "CLIO_LOGBACK_CONF" "CLIO_ENV_FILE" "HOST_CLIO_PORT" "CONTAINER_CLIO_PORT"; do
+    echo "${var}=${!var}" >> ${CTMPL_ENV_FILE}
+done
+
+# copy configs to temp dir
+cp ${CONFIG_DIR}/* ${TMPDIR}/
+
+# render all ctmpls
+docker run --rm \
+    -v ${TMPDIR}:/working \
+    -v ${CLIO_DIR}/jenkins:/scripts \
+    -v ${TOKEN_FILE}:/root/.vault-token:ro \
+    --env-file="${CTMPL_ENV_FILE}" \
+    broadinstitute/dsde-toolbox:latest /scripts/render-ctmpl.sh
+rm -f ${CTMPL_ENV_FILE}
+
+stopcontainer() {
+    # stop running container on host
+    ${SSHCMD} "test -f ${APP_DIR}/docker-compose.yml && docker-compose -p clio-server -f ${APP_DIR}/docker-compose.yml stop || echo"
+
+    # docker-compose rm -f
+    ${SSHCMD} "test -f ${APP_DIR}/docker-compose.yml && docker-compose -p clio-server -f ${APP_DIR}/docker-compose.yml rm -f || echo"
+}
+
+startcontainer() {
+    # docker-compose pull
+    ${SSHCMD} "docker-compose -p clio-server -f ${APP_DIR}/docker-compose.yml pull"
+
+    # docker-compose up -d
+    ${SSHCMD} "docker-compose -p clio-server -f ${APP_DIR}/docker-compose.yml up -d"
+}
+
+# copy configs to temp directory on host
+# rm -rf /app.new
+# mkdir /app.new
+${SSHCMD} "sudo rm -rf ${APP_DIR}.new && sudo mkdir ${APP_DIR}.new && sudo chgrp ${SSH_USER} ${APP_DIR}.new && sudo chmod g+w ${APP_DIR}.new"
+
+# copy configs to /app.new
+${SCPCMD} -r ${TMPDIR}/* ${SSH_USER}@${CLIO_HOST}:${APP_DIR}.new/
+
+# stop running the existing container
+stopcontainer
+
+# move /app.old to /app.old.old
+# move /app to /app.old
+# move /app.new to /app
+${SSHCMD} <<-EOF
+    ([ ! -d ${APP_DIR}.old ] || sudo mv ${APP_DIR}.old ${APP_DIR}.old.old) &&
+    ([ ! -d ${APP_DIR} ] || sudo mv ${APP_DIR} ${APP_DIR}.old) &&
+    sudo mv ${APP_DIR}.new ${APP_DIR}
+EOF
+
+# start running the new container
+startcontainer
+
+# wait for the server to come up before checking its health
+sleep 10
+
+# hit the health and version endpoints to check the deployment status
+STATUS=$(curl -s "${CLIO_HOST}:${HOST_CLIO_PORT}/health" | jq -r .search)
+VERSION=$(curl -s "${CLIO_HOST}:${HOST_CLIO_PORT}/version" | jq -r .version)
+
+if [[ "$STATUS" == "OK" && "$VERSION" == "$DOCKER_TAG" ]]; then
+    # rm /app.old.old
+    ${SSHCMD} "sudo rm -rf ${APP_DIR}.old.old"
+    exit 0
+else
+    # roll back the deployment
+    # stop running the new container
+    stopcontainer
+
+    # rm /app
+    # move /app.old to /app
+    # move /app.old.old to /app.old
+    ${SSHCMD} <<-EOF
+        sudo rm -rf ${APP_DIR} &&
+        ([ ! -d ${APP_DIR}.old ] || sudo mv ${APP_DIR}.old ${APP_DIR}) &&
+        ([ ! -d ${APP_DIR}.old.old ] || sudo mv ${APP_DIR}.old.old ${APP_DIR}.old)
+EOF
+
+    # start running the old container
+    startcontainer
+
+    exit 2
+fi
---
+++ jenkins/render-ctmpl.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+# This script was copied from the gotc project.
+
+progname=`basename $0`
+
+# set defaults
+
+# flag to determine if we base64 decode
+base64=0
+# flag to determine if we keep ctmpls around after render
+keep=0
+
+# default working directory
+output="/working"
+# environment
+ENVIRONMENT=${ENVIRONMENT-""}
+# Vault
+VAULT_TOKEN=${VAULT_TOKEN-`cat /root/.vault-token`}
+# loglevel
+LOG_LEVEL=${LOG_LEVEL-"err"}
+
+export ENVIRONMENT
+export VAULT_TOKEN
+
+# config values
+CONSUL_TEMPLATE_PATH=${CONSUL_TEMPLATE-"/usr/local/bin"}
+CONSUL_CONFIG=${CONSUL_CONFIG-"/etc/consul-template/config/config.json"}
+
+# add getops for
+#  -e ENVIRONMENT
+#  -k keep files
+#  -v VAULT_TOKEN
+#  -l LOG_LEVEL
+#  -w working directory
+
+# filenames to process are rest of args
+# files="$*"
+
+
+# check if required values are missing
+
+# check if output dir exists
+
+usage() {
+    echo "${progname} [-k] [-e ENV] [-v VAULT_TOKEN] [-l LOG_LEVEL] [-w working_directory] file(s)"
+}
+
+errorout() {
+    if [ $1 -ne 0 ]; then
+        echo
+        echo "${2}"
+        exit $1
+    fi
+}
+
+cd ${output}
+
+for file in `ls`
+do
+    # check if it is a file
+    if [ ! -f "${file}" ]
+    then
+        # skip non-regular files
+        continue
+    fi
+
+    # filename without .ctmpl
+    outputfile="${file%.*}"
+    extension="${file##*.}"
+
+    # only process ctmpl files skip all others
+    if [ "${extension}" != "ctmpl" ]
+    then
+        # skip
+        continue
+    fi
+
+    # check if outputfile ends in .b64
+    # get second extension
+    extension2="${outputfile##*.}"
+
+    # set flag and get final file name
+    if [ "${extension2}" == "b64" ]
+    then
+        base64=1
+        finalfile="${outputfile%.*}"
+    fi
+
+    echo "Rendering ${file} to ${outputfile} .."
+
+    ${CONSUL_TEMPLATE_PATH}/consul-template \
+        -once \
+        -config=${CONSUL_CONFIG} \
+        -log-level=${LOG_LEVEL} \
+        -template=$file:$outputfile
+    errorout $? "FATAL ERROR: Unable to execute consule-template for ${file}!"
+
+    if [ "${base64}" -eq "1" ]
+    then
+       echo "Base64 decoding ${outputfile} to ${finalfile}"
+       # base64
+       # since ctmpl will likely create blanklines and base64 does not like
+       # them  - remove them
+       tr -d '\n' < ${outputfile} | base64 -d  > ${finalfile}
+       test "${keep}" -eq 0 && rm -f ${outputfile}
+    fi
+
+    # clean up
+    test "${keep}" -eq 0 && rm -f ${file}
+    # reset base64 flag
+    base64=0
+
+done
+
+exit 0
