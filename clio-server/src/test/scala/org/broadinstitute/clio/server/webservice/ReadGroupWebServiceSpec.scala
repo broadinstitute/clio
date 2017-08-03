89c1dce28802317a7cfc86a13fbcbc289d875f16
Trevyn Langsford <tlangs@broadinstitute.org>
2017-08-03 16:18:41 -0400

    Tl dsdegp 1234 ubam list api (#49)
    * Added test specs to test that web API will return lists of ReadGroups based on non-key search criteria

--- clio-server/src/test/scala/org/broadinstitute/clio/server/webservice/ReadGroupWebServiceSpec.scala
+++ clio-server/src/test/scala/org/broadinstitute/clio/server/webservice/ReadGroupWebServiceSpec.scala
@@ -5,6 +5,8 @@ import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import io.circe.Json
+import org.broadinstitute.clio.server.MockClioApp
+import org.broadinstitute.clio.server.dataaccess.MemoryReadGroupSearchDAO
 import org.broadinstitute.clio.server.service.SchemaService
 import org.broadinstitute.clio.server.webservice.WebServiceAutoDerivation._
 import org.scalatest.{FlatSpec, Matchers}
@@ -32,6 +34,63 @@ class ReadGroupWebServiceSpec
       Map("project" -> "testGCPlocation")
     ) ~> webService.postMetadata ~> check {
       responseAs[Map[String, String]] should be(empty)
+    }
+  }
+
+  it should "query with a project and sample and return multiple records" in {
+    val memorySearchDAO = new MemoryReadGroupSearchDAO()
+    val app = MockClioApp(searchDAO = memorySearchDAO)
+    val webService = new MockReadGroupWebService(app)
+    Post(
+      "/metadata/v1/barcodeGCP/5/LibraryGCP/GCP",
+      Map("project" -> "testProject1", "sample_alias" -> "sample1")
+    ) ~> webService.postMetadata ~> check {
+      status shouldEqual StatusCodes.OK
+    }
+    Post(
+      "/metadata/v1/barcodeGCP/6/LibraryGCP/GCP",
+      Map("project" -> "testProject1", "sample_alias" -> "sample1")
+    ) ~> webService.postMetadata ~> check {
+      status shouldEqual StatusCodes.OK
+    }
+    Post(
+      "/metadata/v1/barcodeGCP/7/LibraryGCP/GCP",
+      Map("project" -> "testProject1", "sample_alias" -> "sample2")
+    ) ~> webService.postMetadata ~> check {
+      status shouldEqual StatusCodes.OK
+    }
+
+    // We have to test the MemorySearchDAO because we're not going to implement
+    // Elasticsearch logic in our test specs. Here, we're just verifying that
+    // the web service passes the appropriate queries onto the search DAO.
+    Post(
+      "/query/v1",
+      Map("project" -> "testProject1")
+    ) ~> webService.query ~> check {
+      memorySearchDAO.queryReadGroupCalls should have length 1
+      val firstQuery = memorySearchDAO.queryReadGroupCalls.head
+      firstQuery.project should be(Some("testProject1"))
+      firstQuery.sampleAlias should be(empty)
+    }
+
+    Post(
+      "/query/v1",
+      Map("project" -> "testProject1", "sample_alias" -> "sample1")
+    ) ~> webService.query ~> check {
+      memorySearchDAO.queryReadGroupCalls should have length 2
+      val secondQuery = memorySearchDAO.queryReadGroupCalls(1)
+      secondQuery.project should be(Some("testProject1"))
+      secondQuery.sampleAlias should be(Some("sample1"))
+    }
+  }
+
+  it should "query with a BoGuS project and sample and return nothing" in {
+    val webService = new MockReadGroupWebService()
+    Post(
+      "/query/v1",
+      Map("project" -> "testBoGuSproject", "sample_alias" -> "testBoGuSsample")
+    ) ~> webService.query ~> check {
+      responseAs[Seq[String]] should be(empty)
     }
   }
 
