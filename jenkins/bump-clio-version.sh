364d4f00661a98e0a676d91a55af6112d2349e1c
Dan Moran <danmoran@broadinstitute.org>
2017-08-11 13:17:43 -0400

    Add explanatory comments.

--- jenkins/bump-clio-version.sh
+++ jenkins/bump-clio-version.sh
@@ -31,21 +31,30 @@ cd "$CLIO_DIR"
 COMMIT=$(git rev-parse HEAD)
 BASE_VERSION=$(cat ${CLIO_DIR}/.clio-version | tr -d '\n')
 
+# By default, `git describe` searches backwards for the closest tag
+# Passing --exact-match makes it only look at HEAD, but then it raises an error if the HEAD is un-tagged
 HEAD_TAG=$(git describe --exact-match || echo)
 
+# We check for the tag dev instead of the branch develop because that's what the stage-RC job does
+# The HEAD of develop might be broken at any given point, but the tag dev is only moved after a successful
+# test & deploy to the dev environment
 if [ "$HEAD_TAG" = dev ]
 then
-  NEXT_MINOR=$(perl -ne 'print $1.".".($2+1).".".0 if /(\d+)\.(\d+)\.(\d+)/' ${VERSION_FILE})
+  # When we cut from dev, we bump the minor component of the base Clio version, and set the patch to 0
+  # Bumping the minor version on develop makes it be always ahead of any version bumps that might happen
+  # on the hotfix branch cut during this release
+  NEXT_MINOR=$(perl -ne 'print $1.".".($2+1).".0" if /(\d+)\.(\d+)\.\d+/' ${VERSION_FILE})
   git checkout develop
   echo "$NEXT_MINOR" > ${VERSION_FILE}
   git commit -a -m "Bump next base version to ${NEXT_MINOR}"
   git checkout "$COMMIT"
 fi
 
+# When we cut from anything other than dev, we assume it's a hotfix branch and bump the patch version
+# We don't touch develop in this case because when the hotfix branch was cut, the minor version would
+# have been bumped in develop, so we can bump the patch version forever and still be "behind" dev
 NEXT_PATCH=$(perl -ne 'print $1.".".$2.".".($3+1) if /(\d+)\.(\d+)\.(\d+)/' ${VERSION_FILE})
 git checkout -b "${RELEASED_VERSION}-hotfix"
 echo "$NEXT_PATCH" > ${VERSION_FILE}
 git commit -a -m "Bump next base version to ${NEXT_PATCH}"
 git checkout "$COMMIT"
-
-exit 0
