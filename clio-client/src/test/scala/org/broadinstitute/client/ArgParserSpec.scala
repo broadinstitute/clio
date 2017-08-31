1df8d9c5a43693bc0922deec4f519f867adc542f
jcarey <jcarey@broadinstitute.org>
2017-08-31 13:44:04 -0400

    Codecy fixes.

--- clio-client/src/test/scala/org/broadinstitute/client/ArgParserSpec.scala
+++ clio-client/src/test/scala/org/broadinstitute/client/ArgParserSpec.scala
@@ -12,21 +12,20 @@ class ArgParserSpec extends BaseClientSpec {
     val parsed = ClioClient.commandParser.detailedParse(
       Array("query-wgs-ubam", "--location", testLocation)
     )(ClioClient.beforeCommandParser)
-    val location: Location = parsed match {
+    val location: Option[Location] = (parsed match {
       case Right((_, _, optCmd)) =>
-        val innerLocation: Location = optCmd.get match {
+        optCmd map {
           case Right((_, query, _, _)) =>
             query
               .asInstanceOf[QueryWgsUbam]
               .transferWgsUbamV1QueryInput
               .location
-              .get
           case Left(_) => fail("Could not parse subcommand.")
         }
-        innerLocation
       case Left(_) => fail("Could not parse outer command.")
-    }
-    location should be(Location.pathMatcher(testLocation))
+    }).flatten
+
+    location should be(Some(Location.pathMatcher(testLocation)))
   }
 
   it should "throw an exception when given an invalid Location type" in {
@@ -35,19 +34,18 @@ class ArgParserSpec extends BaseClientSpec {
     )(ClioClient.beforeCommandParser)
     val errorMessage = parsed match {
       case Right((_, _, optCmd)) =>
-        optCmd.get match {
+        optCmd map {
           case Right((_, query, _, _)) =>
             query
               .asInstanceOf[QueryWgsUbam]
               .transferWgsUbamV1QueryInput
               .location
-              .get
           case Left(error) => error
         }
       case Left(_) => fail("Could not parse outer command.")
     }
     errorMessage should be(
-      "Unknown enum value BadValue for type org.broadinstitute.clio.util.model.Location "
+      Some("Unknown enum value BadValue for type org.broadinstitute.clio.util.model.Location ")
     )
   }
 
@@ -55,42 +53,38 @@ class ArgParserSpec extends BaseClientSpec {
     val parsed = ClioClient.commandParser.detailedParse(
       Array("query-wgs-ubam", "--document-status", testDocumentStatus.toString)
     )(ClioClient.beforeCommandParser)
-    val docStatus: DocumentStatus = parsed match {
+    val docStatus: Option[DocumentStatus] = (parsed match {
       case Right((_, _, optCmd)) =>
-        val innerDocStatus: DocumentStatus = optCmd.get match {
+        optCmd map {
           case Right((_, query, _, _)) =>
             query
               .asInstanceOf[QueryWgsUbam]
               .transferWgsUbamV1QueryInput
               .documentStatus
-              .get
           case Left(_) => fail("Could not parse subcommand.")
         }
-        innerDocStatus
       case Left(_) => fail("Could not parse outer command.")
-    }
-    docStatus should be(testDocumentStatus)
+    }).flatten
+    docStatus should be(Some(testDocumentStatus))
   }
 
   it should "properly parse an OffsetDateTime string" in {
     val parsed = ClioClient.commandParser.detailedParse(
       Array("query-wgs-ubam", "--run-date-start", testRunDateStart.toString)
     )(ClioClient.beforeCommandParser)
-    val runDateStart: OffsetDateTime = parsed match {
+    val runDateStart: Option[OffsetDateTime] = (parsed match {
       case Right((_, _, optCmd)) =>
-        val innerRunDateStart: OffsetDateTime = optCmd.get match {
+        optCmd map {
           case Right((_, query, _, _)) =>
             query
               .asInstanceOf[QueryWgsUbam]
               .transferWgsUbamV1QueryInput
               .runDateStart
-              .get
           case Left(_) => fail("Could not parse subcommand.")
         }
-        innerRunDateStart
       case Left(_) => fail("Could not parse outer command.")
-    }
-    runDateStart should be(testRunDateStart)
+    }).flatten
+    runDateStart should be(Some(testRunDateStart))
   }
 
   it should "properly parse a string into a bearerToken" in {
