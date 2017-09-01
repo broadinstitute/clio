41771fd4f8b33708346dfccd8892766b2a7732d5
Dan Moran <danmoran@broadinstitute.org>
2017-09-01 13:44:49 -0400

    DSDEGP-1540 Add more error messages to clio-client.

--- clio-client/src/test/scala/org/broadinstitute/client/commands/AddWgsUbamSpec.scala
+++ clio-client/src/test/scala/org/broadinstitute/client/commands/AddWgsUbamSpec.scala
@@ -2,17 +2,16 @@ package org.broadinstitute.client.commands
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.headers.OAuth2BearerToken
-import io.circe.{DecodingFailure, ParsingFailure}
 import org.broadinstitute.client.BaseClientSpec
 import org.broadinstitute.clio.client.commands.AddWgsUbam
 
 class AddWgsUbamSpec extends BaseClientSpec {
-  behavior of "AddReadGroupBam"
+  behavior of "AddWgsUbam"
 
   implicit val bearerToken: OAuth2BearerToken = testBearer
 
-  it should "throw a parsing failure if the metadata is not valid json" in {
-    recoverToSucceededIf[ParsingFailure] {
+  it should "fail if the metadata is not valid json" in {
+    recoverToSucceededIf[Exception] {
       succeedingDispatcher.dispatch(
         AddWgsUbam(
           metadataLocation = badMetadataFileLocation,
@@ -22,8 +21,8 @@ class AddWgsUbamSpec extends BaseClientSpec {
     }
   }
 
-  it should "throw a decoding failure if the json is valid but we can't unmarshal it" in {
-    recoverToSucceededIf[DecodingFailure] {
+  it should "fail if the json is valid but we can't unmarshal it" in {
+    recoverToSucceededIf[Exception] {
       succeedingDispatcher.dispatch(
         AddWgsUbam(
           metadataLocation = metadataPlusExtraFieldsFileLocation,
@@ -33,19 +32,17 @@ class AddWgsUbamSpec extends BaseClientSpec {
     }
   }
 
-  it should "return a failing HttpResponse if there was a server error" in {
+  it should "return a failed future if there was a server error" in {
     recoverToSucceededIf[Exception] {
       failingDispatcher
         .dispatch(goodAddCommand)
     }
   }
 
-  it should "return successful HttpResponse if the server response is OK" in {
-
+  it should "return a successful HttpResponse if the server response is OK" in {
     succeedingDispatcher
       .dispatch(goodAddCommand)
       .map(_.status should be(StatusCodes.OK))
-
   }
 
 }
