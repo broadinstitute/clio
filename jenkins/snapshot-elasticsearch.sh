f4664fb2c50714c2070478a5c8a60eaf71e849c1
Tom Lyons <tbl@broadinstitute.org>
2017-09-11 09:05:46 -0400

    Add snapshot_delete().

--- jenkins/snapshot-elasticsearch.sh
+++ jenkins/snapshot-elasticsearch.sh
@@ -7,37 +7,41 @@ set -e
 function help {
     local -r av0="$1" environment="$2" verb="$3"
     local -r -a environments=(dev prod staging)
-    local ok=no t v
-    local -r -a tools=(curl date jq) verbs=(configure list take)
-    local -r -a  usage=(
+    local -r -a tools=(curl date jq)
+    local -r -a verbs=(configure list take delete)
+    local -r -a usage=(
         ''
-        "$av0: Snapshot an Elasticsearch (Es) cluster into GCS."
+        "$av0: Snapshot an Elasticsearch cluster into GCS."
         ''
-        "Usage: $av0 <environment> <verb>"
+        "Usage: $av0 <environment> <verb> [<snapshot> ...]"
         ''
         "Where: <environment> is one of: ${environments[*]}"
-        "       <verb> is one of: ${verbs[@]}."
-        "       configure configures snapshots in the environment"
-        "       list      lists the snapshots"
-        "       take      takes a snapshot now"
+        "       <verb> is one of: ${verbs[*]}, and"
+        "       <snapshot> is a snapshot name from 'list'"
+        "       and where:"
+        "       configure configures snapshots in environment,"
+        "       list      lists the snapshots already taken, and"
+        "       take      takes a snapshot now, and"
+        "       delete    deletes the named snapshots."
         ''
         "Example: $av0 dev list"
         ''
         "BTW, you ran: $*"
         ''
     )
-    for v in ${environments[*]}; do test $v = "$environment" && ok=yes; done
+    local ok=no x
+    for x in ${environments[*]}; do test $x = "$environment" && ok=yes; done
     if test $ok = no
     then
         echo 1>&2 "${av0}: Unrecognized environment: '$environment'"
     fi
     ok=no
-    for v in ${verbs[*]}; do test $v = "$verb" && ok=yes; done
+    for x in ${verbs[*]}; do test $x = "$verb" && ok=yes; done
     if test $ok = no
     then
         echo 1>&2 "${av0}: Unrecognized verb: '$verb'"
     fi
-    for t in "${tools[@]}"; do which $t >/dev/null || ok=no; done
+    for x in "${tools[@]}"; do which $x >/dev/null || ok=no; done
     if test $ok = no
     then
         echo 1>&2 "${av0}: You need these tools on PATH: ${tools[*]}"
@@ -50,6 +54,7 @@ function help {
 }
 
 # Report that $av0 is running the rest of arguments as a command line.
+# Then run that command line and pipe it through 'jq .'.
 #
 function echoRunJq () {
     local -r av0="$1" ; shift
@@ -61,12 +66,11 @@ function echoRunJq () {
     echo "$result" | jq .
 }
 
-# Configure snapshots to $bucket in $environment for $es cluster.
+# Configure snapshots to $bucket in $environment for the $es cluster.
 #
 function snapshot_configure () {
     local -r av0="$1" environment=$2 es=$3 repo=$4
     local -r bucket=broad-gotc-$environment-clio-elasticsearch
-    local -r url=$es/_snapshot/$repo
     local -r data='
     {
         "type": "gcs",
@@ -77,16 +81,14 @@ function snapshot_configure () {
             "application_name": "clio-elasticsearch"
         }
     }'
-    local -a -r command=(curl -s -X PUT --data "$data" $url)
-    echoRunJq "$av0" "${command[@]}"
+    echoRunJq "$av0" curl -s -X PUT --data "$data" $es/_snapshot/$repo
 }
 
 # List the $repo snapshots in $environment for $es cluster.
 #
 function snapshot_list () {
     local -r av0="$1" environment=$2 es=$3 repo=$4
-    local -a -r command=(curl -s $es/_snapshot/$repo/_all)
-    echoRunJq "$av0" "${command[@]}"
+    echoRunJq "$av0" curl -s $es/_snapshot/$repo/_all
 }
 
 # Take a snapshot in $environment for $es cluster.
@@ -94,27 +96,42 @@ function snapshot_list () {
 function snapshot_take () {
     local -r av0="$1" environment=$2 es=$3 repo=$4
     local -r jq='keys|.[]|select(startswith(".")|not)'
-    local -a -r indexes=($(curl -s $es/_aliases | jq --raw-output $jq))
-    local -r now=clio-$(date -u +%Y-%m-%d-%H-%M-%S)
-    local -r url=$es/_snapshot/$repo/$now?wait_for_completion=true
+    local -r snapshot=clio-$(date -u +%Y-%m-%d-%H-%M-%S)
+    local -r url=$es/_snapshot/$repo/$snapshot?wait_for_completion=true
+    local -a -r curl=(curl -s $es/_aliases)
+    local -r indexes=$("${curl[@]}" | jq --raw-output $jq)
+    if test -z "$indexes"
+    then
+        echo 1>&2 $av0: No document indexes in $es
+        echoRunJq "$av0" "${curl[@]}"
+        exit 2
+    fi
     local index indices comma
-    for index in ${indexes[@]}; do indices=$indices$comma$index; comma=,; done
+    for index in $indexes; do indices=$indices$comma$index; comma=,; done
     local -r data='
     {
         "indices": "'$indices'",
         "include_global_state": false
     }'
-    local -a -r command=(curl -s -X PUT --data "$data" $url)
-    echoRunJq "$av0" "${command[@]}"
+    echoRunJq "$av0" curl -s -X PUT --data "$data" $url
+}
+
+# Delete the snapshots named in $5 ... and so on.
+#
+function snapshot_delete () {
+    local -r av0="$1" environment=$2 es=$3 repo=$4 ; shift 4
+    for snap in "$@"
+    do
+        echoRunJq "$av0" curl -s -X DELETE $es/_snapshot/$repo/$snap
+    done
 }
 
 function main () {
     local -r av0="${0##*/}" environment="$1" verb="$2"
-    local -r repo=clio-gcs-repository
+    help "$av0" "$@" ; shift 2
     local -r domain=gotc-$environment.broadinstitute.org
     local -r es=http://elasticsearch1.$domain:9200
-    help "$av0" "$@"
-    snapshot_$verb "$av0" $environment $es $repo
+    snapshot_$verb "$av0" $environment $es clio-gcs-repository "$@"
 }
 
 main "$@"
