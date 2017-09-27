87d1c6efea1d2b1ccfc3b7bd5ba48a0bad642ca7
Dan Moran <danmoran@broadinstitute.org>
2017-09-27 11:36:13 -0400

    Remove now-unnecessary implicit hacks.

--- clio-client/src/main/scala/org/broadinstitute/clio/client/commands/ClioCommand.scala
+++ clio-client/src/main/scala/org/broadinstitute/clio/client/commands/ClioCommand.scala
@@ -5,15 +5,9 @@ import org.broadinstitute.clio.transfer.model._
 import caseapp.{CommandName, Recurse}
 import caseapp.core.help.CommandsHelp
 import caseapp.core.commandparser.CommandParser
-import shapeless.CNil
 
 /**
   * A specific operation to perform against Clio.
-  *
-  * By default, the caseapp command parser for will convert
-  * case class names into kebab case. For example:
-  *
-  *   AddWgsUbam -> add-wbs-ubam
   *
   * The `@Recurse` annotation tells caseapp to flatten the
   * fields of a nested case class into the option list of
@@ -103,25 +97,6 @@ object ClioCommand extends ClioParsers {
   val queryGvcfName = "query-gvcf"
   val moveGvcfName = "move-gvcf"
   val deleteGvcfName = "delete-gvcf"
-
-  /*
-   * Based on its docs / examples, caseapp *should* be able to inductively
-   * derive a `CommandParser[ClioCommand]` without needing this import. For
-   * some reason, though, the implicit values defined in the companion object
-   * of `CommandParser` aren't getting picked up during implicit search like
-   * they should be (on a clean compile in a new JVM at least; something about
-   * the Zinc incremental compiler used by the SBT repl and IntelliJ's Scala
-   * plugin makes compilation work the second time around, even though if you
-   * run "show implicit parameters" on `CommandParser[ClioCommand]` below in
-   * IntelliJ without this import it displays a "no implicit found" error message).
-   *
-   * By importing the implicits from the companion object directly, we get
-   * induction to actually work. The `implicitly` call both demonstrates that
-   * the base-case implicit is properly in-scope and convinces IntelliJ that
-   * the import is actually used for something.
-   */
-  import caseapp.CommandParser._
-  implicitly[CommandParser[CNil]]
 
   /** The caseapp parser to use for all Clio sub-commands. */
   val parser: CommandParser[ClioCommand] =
