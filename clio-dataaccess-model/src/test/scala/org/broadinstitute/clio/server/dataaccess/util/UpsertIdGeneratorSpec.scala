9fcd5b80c95dcd302e9afc9fc953f9e0e5eb19ea
Tom Lyons <tbl@broadinstitute.org>
2017-09-27 12:09:37 -0400

    Wrap ID generation in Future to test thread-safety somewhat.

--- clio-dataaccess-model/src/test/scala/org/broadinstitute/clio/server/dataaccess/util/UpsertIdGeneratorSpec.scala
+++ clio-dataaccess-model/src/test/scala/org/broadinstitute/clio/server/dataaccess/util/UpsertIdGeneratorSpec.scala
@@ -1,23 +1,38 @@
 package org.broadinstitute.clio.server.dataaccess.util
 
 import org.scalatest.{FlatSpec, Matchers}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 class UpsertIdGeneratorSpec extends FlatSpec with Matchers {
   behavior of "UpsertIdGenerator"
 
-  val count = 100000
-  def generateIds(): Seq[String] = Seq.fill(count) {
-    UpsertIdGenerator.nextId()
+  /**
+    * Generate this many IDs.
+    */
+  private val Count = 100000
+
+  /**
+    * Wrap ID generation in a future to test thread-safety somewhat.
+    *
+    * @return a future sequence of IDs
+    */
+  def generateIds(): Future[Seq[String]] = Future.sequence {
+    Seq.fill(Count) {
+      Future(UpsertIdGenerator.nextId())
+    }
   }
 
   it should "generate unique 20-character IDs" in {
-    generateIds()
-      .filter(id => id.length == 20)
-      .distinct should have length count.toLong
+    generateIds().map { ids =>
+      ids.filter(id => id.length == 20).distinct should have length Count.toLong
+    }
   }
 
   it should "generate IDs in ascending order" in {
-    val ids = generateIds()
-    ids.sortWith(_.compareTo(_) < 0) should be(ids)
+    val fids = generateIds()
+    fids.map { ids =>
+      ids.sortWith(_.compareTo(_) < 0) should be(ids)
+    }
   }
 }
