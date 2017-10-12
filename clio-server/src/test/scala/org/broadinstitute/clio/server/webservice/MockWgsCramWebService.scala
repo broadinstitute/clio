3afa95c44e434f72c2985173847587a6f784b553
Daniel Moran <danxmoran@gmail.com>
2017-10-11 22:25:19 -0400

    DSDEGP-1536 Add WgsCramWebServiceSpec. (#146)

---
+++ clio-server/src/test/scala/org/broadinstitute/clio/server/webservice/MockWgsCramWebService.scala
@@ -0,0 +1,21 @@
+package org.broadinstitute.clio.server.webservice
+
+import org.broadinstitute.clio.server.service.{
+  PersistenceService,
+  SearchService,
+  WgsCramService
+}
+import org.broadinstitute.clio.server.{ClioApp, MockClioApp}
+
+import scala.concurrent.ExecutionContext
+
+class MockWgsCramWebService(
+  app: ClioApp = MockClioApp()
+)(implicit executionContext: ExecutionContext)
+    extends WgsCramWebService
+    with JsonWebService {
+  private val persistence = PersistenceService(app)
+  private val search = SearchService(app)
+  override lazy val wgsCramService: WgsCramService =
+    new WgsCramService(persistence, search)
+}
--- clio-server/src/test/scala/org/broadinstitute/clio/server/webservice/WgsCramWebServiceSpec.scala
+++ clio-server/src/test/scala/org/broadinstitute/clio/server/webservice/WgsCramWebServiceSpec.scala
@@ -2,29 +2,29 @@ package org.broadinstitute.clio.server.webservice
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
+import com.sksamuel.elastic4s.searches.queries.BoolQueryDefinition
 import io.circe.Json
 import org.broadinstitute.clio.server.MockClioApp
 import org.broadinstitute.clio.server.dataaccess.MemorySearchDAO
-import org.broadinstitute.clio.server.dataaccess.elasticsearch.DocumentGvcf
+import org.broadinstitute.clio.server.dataaccess.elasticsearch.DocumentWgsCram
+import org.broadinstitute.clio.server.service.WgsCramService
+import org.broadinstitute.clio.transfer.model.WgsCramIndex
+import org.broadinstitute.clio.transfer.model.wgscram.TransferWgsCramV1QueryInput
 import org.broadinstitute.clio.util.model.{DocumentStatus, Location, UpsertId}
-import com.sksamuel.elastic4s.searches.queries.BoolQueryDefinition
-import org.broadinstitute.clio.server.service.GvcfService
-import org.broadinstitute.clio.transfer.model.GvcfIndex
-import org.broadinstitute.clio.transfer.model.gvcf.TransferGvcfV1QueryInput
 
-class GvcfWebServiceSpec extends BaseWebserviceSpec {
-  behavior of "GvcfWebService"
+class WgsCramWebServiceSpec extends BaseWebserviceSpec {
+  behavior of "WgsCramWebService"
 
   it should "postMetadata with OnPrem location" in {
-    val webService = new MockGvcfWebService()
-    Post("/metadata/OnPrem/proj0/sample_alias0/1", Map("notes" -> "some note")) ~> webService.gvcfPostMetadata ~> check {
+    val webService = new MockWgsCramWebService()
+    Post("/metadata/OnPrem/proj0/sample_alias0/1", Map("notes" -> "some note")) ~> webService.cramPostMetadata ~> check {
       UpsertId.isValidId(responseAs[String]) should be(true)
     }
   }
 
   it should "postMetadata with GCP location" in {
-    val webService = new MockGvcfWebService()
-    Post("/metadata/GCP/proj0/sample_alias0/1", Map("notes" -> "some note")) ~> webService.gvcfPostMetadata ~> check {
+    val webService = new MockWgsCramWebService()
+    Post("/metadata/GCP/proj0/sample_alias0/1", Map("notes" -> "some note")) ~> webService.cramPostMetadata ~> check {
       UpsertId.isValidId(responseAs[String]) should be(true)
     }
   }
@@ -32,44 +32,44 @@ class GvcfWebServiceSpec extends BaseWebserviceSpec {
   it should "query with a project and sample and return multiple records" in {
     val memorySearchDAO = new MemorySearchDAO()
     val app = MockClioApp(searchDAO = memorySearchDAO)
-    val webService = new MockGvcfWebService(app)
+    val webService = new MockWgsCramWebService(app)
     Post(
       "/metadata/OnPrem/proj0/sample_alias0/1",
-      Map("notes" -> "some note", "gvcf_path" -> "gs://path/gvcf.gvcf")
-    ) ~> webService.gvcfPostMetadata ~> check {
+      Map("notes" -> "some note", "cram_path" -> "gs://path/cram.cram")
+    ) ~> webService.cramPostMetadata ~> check {
       status shouldEqual StatusCodes.OK
     }
     Post(
       "/metadata/OnPrem/proj1/sample_alias0/1",
-      Map("notes" -> "some note", "gvcf_path" -> "gs://path/gvcf.gvcf")
-    ) ~> webService.gvcfPostMetadata ~> check {
+      Map("notes" -> "some note", "cram_path" -> "gs://path/cram.cram")
+    ) ~> webService.cramPostMetadata ~> check {
       status shouldEqual StatusCodes.OK
     }
     Post(
       "/metadata/OnPrem/proj2/sample_alias0/1",
-      Map("notes" -> "some note", "gvcf_path" -> "gs://path/gvcf.gvcf")
-    ) ~> webService.gvcfPostMetadata ~> check {
+      Map("notes" -> "some note", "cram_path" -> "gs://path/cram.cram")
+    ) ~> webService.cramPostMetadata ~> check {
       status shouldEqual StatusCodes.OK
     }
 
     // We have to test the MemorySearchDAO because we're not going to implement
     // Elasticsearch logic in our test specs. Here, we're just verifying that
     // the web service passes the appropriate queries onto the search DAO.
-    Post("/query", Map("project" -> "proj0")) ~> webService.gvcfQuery ~> check {
+    Post("/query", Map("project" -> "proj0")) ~> webService.cramQuery ~> check {
       memorySearchDAO.queryCalls should be(
         Seq(
-          TransferGvcfV1QueryInput(
+          TransferWgsCramV1QueryInput(
             project = Some("proj0"),
             documentStatus = Some(DocumentStatus.Normal)
           )
-        ).map(GvcfService.v1QueryConverter.buildQuery)
+        ).map(WgsCramService.v1QueryConverter.buildQuery)
       )
     }
 
     Post(
       "/query",
       Map("project" -> "testProject1", "sample_alias" -> "sample1")
-    ) ~> webService.gvcfQuery ~> check {
+    ) ~> webService.cramQuery ~> check {
       memorySearchDAO.queryCalls should have length 2
       val secondQuery =
         memorySearchDAO.queryCalls(1).asInstanceOf[BoolQueryDefinition]
@@ -80,15 +80,15 @@ class GvcfWebServiceSpec extends BaseWebserviceSpec {
   it should "upsert a record, delete it and then fail to find it with query, but find it with queryall" in {
     val memorySearchDAO = new MemorySearchDAO()
     val app = MockClioApp(searchDAO = memorySearchDAO)
-    val webService = new MockGvcfWebService(app)
+    val webService = new MockWgsCramWebService(app)
     Post(
       "/metadata/GCP/proj1/sample_alias0/1",
       Map(
-        "gvcf_md5" -> "abcgithashdef",
+        "cram_md5" -> "abcgithashdef",
         "notes" -> "some note",
-        "gvcf_path" -> "gs://path/gvcf.gvcf"
+        "cram_path" -> "gs://path/cram.cram"
       )
-    ) ~> webService.gvcfPostMetadata ~> check {
+    ) ~> webService.cramPostMetadata ~> check {
       status shouldEqual StatusCodes.OK
       memorySearchDAO.updateCalls should have length 1
       val firstUpdate = memorySearchDAO.updateCalls.headOption
@@ -98,103 +98,103 @@ class GvcfWebServiceSpec extends BaseWebserviceSpec {
           // scolds us for using .head
           fail("Impossible because of the above check")
         }
-        .asInstanceOf[DocumentGvcf]
+        .asInstanceOf[DocumentWgsCram]
 
       firstUpdate.upsertId should be(responseAs[UpsertId])
-      firstUpdate.gvcfMd5 should be(Some("abcgithashdef"))
+      firstUpdate.cramMd5 should be(Some("abcgithashdef"))
       firstUpdate.notes should be(Some("some note"))
-      firstUpdate.gvcfPath should be(Some("gs://path/gvcf.gvcf"))
+      firstUpdate.cramPath should be(Some("gs://path/cram.cram"))
     }
 
     // We have to test the MemorySearchDAO because we're not going to implement
     // Elasticsearch logic in our test specs. Here, we're just verifying that
     // the web service passes the appropriate queries onto the search DAO.
-    Post("/query", Map("location" -> "GCP")) ~> webService.gvcfQuery ~> check {
+    Post("/query", Map("location" -> "GCP")) ~> webService.cramQuery ~> check {
       memorySearchDAO.queryCalls should be(
         Seq(
-          TransferGvcfV1QueryInput(
+          TransferWgsCramV1QueryInput(
             location = Some(Location.GCP),
             documentStatus = Some(DocumentStatus.Normal)
           )
-        ).map(GvcfService.v1QueryConverter.buildQuery)
+        ).map(WgsCramService.v1QueryConverter.buildQuery)
       )
     }
 
     Post(
       "/metadata/GCP/proj0/alias/2",
       Map(
-        "gvcf_md5" -> "abcgithashdef",
+        "cram_md5" -> "abcgithashdef",
         "notes" -> "some note",
         "document_status" -> "Deleted",
-        "gvcf_path" -> ""
+        "cram_path" -> ""
       )
-    ) ~> webService.gvcfPostMetadata ~> check {
+    ) ~> webService.cramPostMetadata ~> check {
       status shouldEqual StatusCodes.OK
       memorySearchDAO.updateCalls should have length 2
       val secondUpdate = memorySearchDAO.updateCalls
         .map(_._1)
         .apply(1)
-        .asInstanceOf[DocumentGvcf]
-      secondUpdate.gvcfMd5 should be(Some("abcgithashdef"))
+        .asInstanceOf[DocumentWgsCram]
+      secondUpdate.cramMd5 should be(Some("abcgithashdef"))
       secondUpdate.notes should be(Some("some note"))
       secondUpdate.documentStatus should be(Some(DocumentStatus.Deleted))
-      secondUpdate.gvcfPath should be(Some(""))
+      secondUpdate.cramPath should be(Some(""))
     }
 
     // We have to test the MemorySearchDAO because we're not going to implement
     // Elasticsearch logic in our test specs. Here, we're just verifying that
     // the web service passes the appropriate queries onto the search DAO.
-    Post("/queryall", Map("location" -> "GCP")) ~> webService.gvcfQueryall ~> check {
+    Post("/queryall", Map("location" -> "GCP")) ~> webService.cramQueryall ~> check {
       memorySearchDAO.queryCalls should be(
         Seq(
           // From /query call earlier in the test.
-          TransferGvcfV1QueryInput(
+          TransferWgsCramV1QueryInput(
             location = Some(Location.GCP),
             documentStatus = Some(DocumentStatus.Normal)
           ),
           // No documentStatus restriction from /queryall
-          TransferGvcfV1QueryInput(location = Some(Location.GCP))
-        ).map(GvcfService.v1QueryConverter.buildQuery)
+          TransferWgsCramV1QueryInput(location = Some(Location.GCP))
+        ).map(WgsCramService.v1QueryConverter.buildQuery)
       )
     }
   }
 
   it should "query with a BoGuS project and sample and return nothing" in {
-    val webService = new MockGvcfWebService()
+    val webService = new MockWgsCramWebService()
     Post(
       "/query",
       Map("project" -> "testBoGuSproject", "sample_alias" -> "testBoGuSsample")
-    ) ~> webService.gvcfQuery ~> check {
+    ) ~> webService.cramQuery ~> check {
       responseAs[Seq[String]] should be(empty)
     }
   }
 
   it should "reject postMetadata with BoGuS location" in {
-    val webService = new MockGvcfWebService()
+    val webService = new MockWgsCramWebService()
     Post("/metadata/BoGuS/proj0/alias/2", Map("project" -> "testBoGuSlocation")) ~> Route
-      .seal(webService.gvcfPostMetadata) ~> check {
+      .seal(webService.cramPostMetadata) ~> check {
       status shouldEqual StatusCodes.NotFound
     }
   }
 
   it should "query with an empty request" in {
-    val webService = new MockGvcfWebService()
-    Post("/query", Map.empty[String, String]) ~> webService.gvcfQuery ~> check {
+    val webService = new MockWgsCramWebService()
+    Post("/query", Map.empty[String, String]) ~> webService.cramQuery ~> check {
       responseAs[Seq[String]] should be(empty)
     }
   }
 
   it should "query without an empty request" in {
-    val webService = new MockGvcfWebService()
-    Post("/query", Map("project" -> "testProject")) ~> webService.gvcfQuery ~> check {
+    val webService = new MockWgsCramWebService()
+    Post("/query", Map("project" -> "testProject")) ~> webService.cramQuery ~> check {
       responseAs[Seq[String]] should be(empty)
     }
   }
 
   it should "return a JSON schema" in {
-    val webService = new MockGvcfWebService()
-    Get("/schema") ~> webService.gvcfGetSchema ~> check {
-      responseAs[Json] should be(GvcfIndex.jsonSchema)
+    val webService = new MockWgsCramWebService()
+    Get("/schema") ~> webService.cramGetSchema ~> check {
+      responseAs[Json] should be(WgsCramIndex.jsonSchema)
     }
   }
 }
