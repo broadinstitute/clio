dc852f84a426058ec9b13fabaa1064e3b44a4f56
Dan Moran <danmoran@broadinstitute.org>
2017-11-06 09:56:35 -0500

    Remove parallel marshalling to avoid overloading on concurrent queries.

--- clio-server/src/main/scala/org/broadinstitute/clio/server/webservice/JsonWebService.scala
+++ clio-server/src/main/scala/org/broadinstitute/clio/server/webservice/JsonWebService.scala
@@ -17,23 +17,14 @@ trait JsonWebService
 
   /**
     * Settings configuring how web-service classes will render JSON
-    * streams into HTTP responses.
+    * streams into HTTP response entities.
     *
     * By default, JSON streams are sent to the client as valid JSON
     * arrays, with elements marshalled into byte strings one-by-one.
     *
-    * We override the config so multiple stream elements can be marshalled
-    * in parallel, but maintain the array structure and ordering to avoid
-    * breaking anything downstream.
-    *
-    * More customizations are possible, see:
+    * Customizations are possible, see:
     * https://doc.akka.io/docs/akka-http/current/scala/http/routing-dsl/source-streaming-support.html#customising-response-rendering-mode
     */
   final implicit val jsonStreamingSupport: JsonEntityStreamingSupport =
-    EntityStreamingSupport
-      .json()
-      .withParallelMarshalling(
-        parallelism = Runtime.getRuntime.availableProcessors() / 2,
-        unordered = false
-      )
+    EntityStreamingSupport.json()
 }
