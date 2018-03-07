d3b4a635e9da6baf429394f903bff13583879932
Tom Lyons <tbl3rd@gmail.com>
2018-03-07 12:08:10 -0500

    DSDEGP-2010 Create a new index object for Arrays (#222)
    * Define ArraysIndex object.
    * Add Arrays commands to client.
    * Define and (client) test Add command for Arrays.
    * Test Delete command for Arrays in client.
    * Test Schema command for Arrays in client.
    * Test Move command for Arrays in client a little.
    * Fuss to improve consistency.
    * Name consistently and according to Dan's review comments.

---
+++ clio-client/src/test/scala/org/broadinstitute/clio/client/dispatch/AddArraysSpec.scala
@@ -0,0 +1,45 @@
+package org.broadinstitute.clio.client.dispatch
+
+import akka.http.scaladsl.model.headers.OAuth2BearerToken
+import org.broadinstitute.clio.client.BaseClientSpec
+import org.broadinstitute.clio.client.commands.AddArrays
+
+class AddArraysSpec extends BaseClientSpec {
+  behavior of "AddArrays"
+
+  implicit val bearerToken: OAuth2BearerToken = testBearer
+
+  val dispatcher = succeedingDispatcher()
+
+  it should "fail if the metadata is not valid json" in {
+    recoverToSucceededIf[IllegalArgumentException] {
+      dispatcher.dispatch(
+        AddArrays(
+          metadataLocation = badMetadataFileLocation,
+          key = testArraysTransferV1Key
+        )
+      )
+    }
+  }
+
+  it should "fail if the json is valid but we can't decode it" in {
+    recoverToSucceededIf[IllegalArgumentException] {
+      dispatcher.dispatch(
+        AddArrays(
+          metadataLocation = metadataPlusExtraFieldsFileLocation,
+          key = testArraysTransferV1Key
+        )
+      )
+    }
+  }
+
+  /**
+    * FIXME: Bogus because server does not support Arrays yet.
+    */
+  it should "return a failed future if there was a server error" in {
+    recoverToSucceededIf[RuntimeException] {
+      failingDispatcher
+        .dispatch(goodArraysAddCommand)
+    }
+  }
+}
