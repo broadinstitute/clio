0d40b7927dae52899e940d5f40fa8aad6eb56bb7
Tom Lyons <tbl@broadinstitute.org>
2020-01-17 15:43:13 -0500

    Not sure what I fixed, but ...

--- clio-client/src/test/scala/org/broadinstitute/clio/client/dispatch/RetrieveAndPrintExecutorSpec.scala
+++ clio-client/src/test/scala/org/broadinstitute/clio/client/dispatch/RetrieveAndPrintExecutorSpec.scala
@@ -58,18 +58,24 @@ class RetrieveAndPrintExecutorSpec extends BaseClientSpec with AsyncMockFactory 
       .map(_.toString)
       .runWith(Sink.head)
       .map(s => println(s"orElse == '$s'"))
-    Future(true should be(false))
+    Source
+      .empty[Json]
+      .map(_.spaces2)
+      .intersperse("[", ",\n", "]")
+      // .orElse(Source.single("[]"))
+      .runWith(Sink.foreach(s => println(s"foreach == '$s'")))
+    Future(true should be(true))
   }
 
   it should "return JSON [] when no results" in {
     val query = ArraysQueryInput(chipwellBarcode = Some(Symbol("abcd")))
     val webClient = mock[ClioWebClient]
-    (
-      webClient
-        .simpleQuery(_: ClioWebClient.QueryAux[ArraysQueryInput])(
-          _: ArraysQueryInput,
-          _: Boolean
-        )
+      (
+        webClient
+          .simpleQuery(_: ClioWebClient.QueryAux[ArraysQueryInput])(
+            _: ArraysQueryInput,
+            _: Boolean
+          )
       )
       .expects(ArraysIndex, query, true)
       .returning(Source.empty[Json])
@@ -81,18 +87,17 @@ class RetrieveAndPrintExecutorSpec extends BaseClientSpec with AsyncMockFactory 
           ()
         }
       )
-    val result = executor
+    executor
       .execute(webClient, stub[IoUtil])
       .runWith(Sink.ignore)
-      .map(_ => stdout.toString.trim should be("[]"))
-    result
+      .map(_ => stdout.toString.trim shouldEqual "[]")
   }
 
   it should "retrieve and print server health" in {
     val health = StatusInfo(ClioStatus.Started, SearchStatus.Error)
 
     val webClient = mock[ClioWebClient]
-    (webClient.getClioServerHealth _).expects().returning(Source.single(health.asJson))
+      (webClient.getClioServerHealth _).expects().returning(Source.single(health.asJson))
 
     val stdout = mutable.StringBuilder.newBuilder
     val executor = new RetrieveAndPrintExecutor(GetServerHealth, { s =>
@@ -110,7 +115,7 @@ class RetrieveAndPrintExecutorSpec extends BaseClientSpec with AsyncMockFactory 
     val version = VersionInfo("the-version")
 
     val webClient = mock[ClioWebClient]
-    (webClient.getClioServerVersion _).expects().returning(Source.single(version.asJson))
+      (webClient.getClioServerVersion _).expects().returning(Source.single(version.asJson))
 
     val stdout = mutable.StringBuilder.newBuilder
     val executor = new RetrieveAndPrintExecutor(GetServerVersion, { s =>
@@ -142,12 +147,12 @@ class RetrieveAndPrintExecutorSpec extends BaseClientSpec with AsyncMockFactory 
 
       val webClient = mock[ClioWebClient]
       // Type annotations needed for scalamockery.
-      (
-        webClient
-          .simpleQuery(_: ClioWebClient.QueryAux[UbamQueryInput])(
-            _: UbamQueryInput,
-            _: Boolean
-          )
+        (
+          webClient
+            .simpleQuery(_: ClioWebClient.QueryAux[UbamQueryInput])(
+              _: UbamQueryInput,
+              _: Boolean
+            )
         )
         .expects(UbamIndex, query, includeDeleted)
         .returning(Source(keys.map(_.asJson)))
@@ -156,8 +161,8 @@ class RetrieveAndPrintExecutorSpec extends BaseClientSpec with AsyncMockFactory 
       val executor =
         new RetrieveAndPrintExecutor(QueryUbam(query, includeDeleted, includeDeleted), {
           s =>
-            stdout.append(s)
-            ()
+          stdout.append(s)
+          ()
         })
 
       executor.execute(webClient, stub[IoUtil]).runWith(Sink.seq).map { jsons =>
@@ -179,8 +184,8 @@ class RetrieveAndPrintExecutorSpec extends BaseClientSpec with AsyncMockFactory 
     }
 
     val webClient = mock[ClioWebClient]
-    (webClient
-      .query(_: UbamIndex.type)(_: Json, _: Boolean))
+      (webClient
+        .query(_: UbamIndex.type)(_: Json, _: Boolean))
       .expects(UbamIndex, rawQuery, true)
       .returning(Source(keys.map(_.asJson)))
 
